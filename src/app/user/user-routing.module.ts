32104b0dbd81939037aba9e84bba0da36dc1ebde
Aryan1606 <aryansrivastava1606@gmail.com>
2021-05-09 00:01:03 +0530

    Update

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent} from './create/create.component'
 import { ViewComponent } from './view/view.component'
-import {ViewFormComponent} from './view-form/view-form.component'
+
 const routes: Routes = [
   {
     path:'users/create/view', redirectTo: 'users/view', pathMatch: 'full'
@@ -13,18 +13,6 @@ const routes: Routes = [
   {
     path: 'users/view', component: ViewComponent
   },
-  // {
-  //   path:'viewform', redirectTo: 'users/viewform', pathMatch: 'full'
-  // },
-  // {
-  //   path: 'users/viewform', component: ViewFormComponent
-  // },
-  // {
-  //   path:'users/create/viewform', redirectTo: 'users/viewform', pathMatch: 'full'
-  // },
-  // {
-  //   path: 'users/viewform', component: ViewFormComponent
-  // },
   { 
     path:'', redirectTo: 'users/create', pathMatch: 'full'
   },
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { UserRoutingModule } from './user-routing.module';
 import { CreateComponent } from './create/create.component';
 import { ViewComponent } from './view/view.component';
-// import { UserComponent } from './user.component'
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { ViewFormComponent } from './view-form/view-form.component';
 import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
     CreateComponent,
     ViewComponent,
-    ViewFormComponent,
-    // UserComponent
   ],
   imports: [
     CommonModule,
